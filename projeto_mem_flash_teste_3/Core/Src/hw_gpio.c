dfba79891f1d86d3db8a530ed60fc9e7cc7ec7b5
Luana Izy Veras Tavares <100386237+luanaizy@users.noreply.github.com>
2023-08-17 21:57:42 -0300

    Update hw_gpio.c
    doxygen comments

--- projeto_mem_flash_teste_3/Core/Src/hw_gpio.c
+++ projeto_mem_flash_teste_3/Core/Src/hw_gpio.c
@@ -1,6 +1,6 @@
 /*
- * gpio.c
- *
+ * @file: hw_gpio.c
+ * @brief função de escrita no gpio.
  *  Created on: 24 de jul de 2023
  *      Author: luana
  */
@@ -8,6 +8,13 @@
 
 #include "hw_gpio.h"
 
+/**
+ * @brief função hw_gpio_write_pin - função bloqueante que escreve no gpio.
+ * @param ponteiro para porta do gpio do tipo void.
+ * @param uint16_t pino do gpio.
+ * @param uint32_t com o valor a ser escrito no gpio.
+ * @retval void
+ */
 void hw_gpio_write_pin(void*gpio_port, uint16_t gpio_pin, uint32_t value){
 
 	HAL_GPIO_WritePin((GPIO_TypeDef *)gpio_port, gpio_pin, value);
