896a4947b31f3919a060b33d6472413534d7f146
Luana Izy Veras Tavares <100386237+luanaizy@users.noreply.github.com>
2023-08-17 21:55:24 -0300

    Update hw_spi.c
    doxygen comments

--- projeto_mem_flash_teste_3/Core/Src/hw_spi.c
+++ projeto_mem_flash_teste_3/Core/Src/hw_spi.c
@@ -1,6 +1,6 @@
 /*
- * drive_especifico_mem_flash.c
- *
+ * @file: hw_spi.c
+ * @brief funções de transmissão e recepção spi
  *  Created on: 28 de jun de 2023
  *      Author: luana
  */
@@ -8,7 +8,14 @@
 
 #include "hw_spi.h"
 
-
+/**
+ * @brief função hw_spi_receive - função bloqueante que recebe dados via spi.
+ * @param ponteiro para estrutura hspi do tipo void.
+ * @param ponteiro para vetor para recepção dos dados do tipo uint8_t.
+ * @param uint16_t com o tamanho do dado a ser recebido em bytes.
+ * @param uint32_t com o tempo de timeout da função.
+ * @retval status_hw de erro.
+ */
 status_hw hw_spi_receive(void*hspi, uint8_t *pdata, uint16_t size, uint32_t timeout){
 	int errorcode;
 	errorcode = HAL_SPI_Receive((SPI_HandleTypeDef *)hspi, pdata, size, timeout);
@@ -18,7 +25,14 @@ status_hw hw_spi_receive(void*hspi, uint8_t *pdata, uint16_t size, uint32_t time
 		return error_ok;
 }
 
-
+/**
+ * @brief função hw_spi_transmit - função bloqueante que transmite dados via spi.
+ * @param ponteiro para estrutura hspi do tipo void.
+ * @param ponteiro para vetor com os dados a serem transmitidos do tipo uint8_t
+ * @param uint16_t com o tamanho do dado a ser transmitido em bytes.
+ * @param uint32_t com o tempo de timeout da função.
+ * @retval status hw de erro.
+ */
 status_hw hw_spi_transmit(void *hspi, uint8_t*pdata, uint16_t size, uint32_t timeout){
 	int errorcode;
 	errorcode = HAL_SPI_Transmit((SPI_HandleTypeDef *)hspi, pdata, size, timeout);
