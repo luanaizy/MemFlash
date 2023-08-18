46dd325ccec6444590e1eb095364059e3700a99b
Luana Izy Veras Tavares <100386237+luanaizy@users.noreply.github.com>
2023-08-17 21:50:06 -0300

    Update drive_mem_flash.c
    doxygen comments

--- projeto_mem_flash_teste_3/Core/Src/drive_mem_flash.c
+++ projeto_mem_flash_teste_3/Core/Src/drive_mem_flash.c
@@ -1,5 +1,5 @@
-/*
- *  drive_mem_flash.c
+/**
+ * @file: drive_mem_flash.c
  *
  *  Created on: 28 de jun de 2023
  *      Author: luana
@@ -20,8 +20,15 @@
 
 #define SPI_TIMEOUT                  (1000)/* @brief Timeout das funções de leitura e escrita SPI em ms*/
 
-#define GET_BYTE(addr, num_byte) (addr & (0x000000FF << (8*num_byte)))
+#define GET_BYTE(addr, num_byte) (addr & (0x000000FF << (8*num_byte))) /*função para ajuste dos arrays enviados via spi*/
 
+/**
+ * @brief função estática de writeEnable - habilita a escrita na memória.
+ * @brief é necessário executar o writeEnable antes de qualquer instrução de pageProgram, quadPageProgram, sectorErase, blockErase, chipErase, writeStatusRegister e erase/programSecurityRegisters
+ * @brief neste drive, ele já está embutido nas funções.
+ * @param ponteiro para estrutura de memória do tipo mem_hw_t
+ * @retval void
+ */
 static void writeEnable(mem_hw_t *mem)
 {
 	uint8_t writeenable[1]={MEM_CMD_WRITE_ENABLE};
@@ -30,6 +37,15 @@ static void writeEnable(mem_hw_t *mem)
 	hw_gpio_write_pin(mem->gpio_port, mem->gpio_pin, 1);
 }
 
+/**
+ * @brief função de sectorErase - apaga (leva os bits para 1) um setor da memória (16 páginas de 256 bytes, ou seja, 4Kbytes).
+ * @brief a função de writeEnable é executada dentro desta função.
+ * @brief para um endereço de memória Oxabcdef, o setor apagado será o Oxabc.
+ * @brief Nessa função, o valor do BUSY bit é constantemente consultado por meio da função readStatusRegister1. Quando o sectorErase é finalizado, o BUSY bit vai para zero e a função termina.
+ * @param ponteiro para estrutura de memória do tipo mem_hw_t
+ * @param dado uint32_t para o endereço de memória cujo setor será apagado. A memória tem 4.194.304 endereços, ou seja, o valor máximo para um endereço válido é de 0x3FFFFF
+ * @retval void
+ */
 void sectorErase(mem_hw_t *mem, uint32_t addr)
 {
 
@@ -63,7 +79,18 @@ void sectorErase(mem_hw_t *mem, uint32_t addr)
 
 }
 
-
+/**
+ * @brief função de pageProgram - escreve em de 1 a 256 bytes (uma página) da memória em endereços previamente apagados (levados a 1).
+ * @brief a função de writeEnable é executada dentro desta função.
+ * @brief para um endereço de memória Oxabcdef, a página que terá seu conteúdo escrito será a Oxabcd.
+ * @brief Se uma página inteira será programada (256bytes), os 8 bits menos significativos do endereço devem ser zero. Se numa escrita na memória o número de clocks excede o restante de espaço disponível, a escrita continuará no início daquela mesma página.
+ * @brief Nessa fúnção, o valor do BUSY bit é constantemente consultado por meio da função readStatusRegister1. Quando o pageProgram é finalizado, o BUSY bit vai para zero e a função termina.
+ * @param ponteiro para estrutura de memória do tipo mem_hw_t.
+ * @param dado uint32_t para o endereço de memória no qual a escrita terá início. A memória tem 4.194.304 endereços, ou seja, o valor máximo para um endereço válido é de Ox3FFFFF.
+ * @param ponteiro para vetor com os dados a serem escritos do tipo uint8_t
+ * @param quantidade de bytes a serem escritos
+ * @retval void
+ */
 void pageProgram(mem_hw_t *mem, uint32_t addr, uint8_t *pdata,  uint16_t bytes_length )
 {
 
@@ -94,7 +121,14 @@ void pageProgram(mem_hw_t *mem, uint32_t addr, uint8_t *pdata,  uint16_t bytes_l
 
 }
 
-
+/**
+ * @brief função de readData - permite que um ou mais bytes de dados sejam lidos sequencialmente da memória. Toda a memória pode ser lida com uma única instrução.
+ * @param ponteiro para estrutura de memória do tipo mem_hw_t.
+ * @param dado uint32_t para o endereço de memória no qual a escrita terá início. A memória tem 4.194.304 endereços, ou seja, o valor máximo para um endereço válido é de 0x3FFFFF
+ * @param ponteiro para vetor para os dados a serem recebidos do tipo uint8_t
+ * @param quantidade de bytes a serem recebidos
+ * @retval void
+ */
 
 void readData(mem_hw_t *mem, uint32_t addr, uint8_t *received ,uint16_t size)
 {
@@ -111,6 +145,11 @@ void readData(mem_hw_t *mem, uint32_t addr, uint8_t *received ,uint16_t size)
 
 }
 
+/**
+ * @brief função de jedecId - retorna o Manufacturer ID for Winbond(EFh) e o Device ID.
+ * @param ponteiro para estrutura de memória do tipo mem hw_t.
+ * @retval uint32 t JedecId da memória.
+ */
 uint32_t jedecId(mem_hw_t *mem)
 {
 	uint8_t jedecid[1]={MEM_CMD_JEDEC_ID};
